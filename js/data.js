3b431abb8e1365c5a350202a426d9693ff54ff34
fidesbcmsft <fidesbankcardmicrosoftu@gmail.com>
2023-01-04 14:52:08 -0500

    questions update

--- js/data.js
+++ js/data.js
@@ -12,74 +12,74 @@ export const questions = [
   },
   {
     number: '02',
-    question: '해외 출장이 잡혔다!<br>전날 잠들기 전 나는?',
+    question: 'What is the best part about interacting with customers?',
     choices: [
-      { text: '짐 다 챙겼고 일찍 자야지~', value: 's' },
-      { text: '다 챙겼겠지?<br>여분 옷을 좀 더 챙길까..(뒤척뒤척)', value: 'n' }
+      { text: 'I want to know everything about my customers no matter how long it takes!', value: 's' },
+      { text: 'I want to split my time equally between all my customers.', value: 'n' }
     ]
   },
   {
     number: '03',
-    question: '친했던 동기가<br>퇴사를?',
+    question: 'What is the most important part of your business?',
     choices: [
-      { text: '일하면서 많이 힘들었나?<br>내가 좀 더 챙겨줄걸.', value: 'f' },
-      { text: '아 이직하나?<br>퇴사하고 뭐하냐고 물어봐야지.', value: 't' }
+      { text: 'Delivering the highest quality product or service to my customers!', value: 'f' },
+      { text: 'I want to focus on my customer satisfaction and retention.', value: 't' }
     ]
   },
   {
     number: '04',
-    question: '대규모 프로젝트를 맡게 되어 떨리는 마음에 처음 시작한 건?',
+    question: 'How do you deal with roadblocks and problems while building a business?',
     choices: [
-      { text: '큰 프로젝트인 만큼 계획이 중요해!<br>목적과 목표 먼저 정리해봐야지!', value: 'j' },
-      { text: '당장 미팅 잡고<br>팀원이랑 이야기하면 되겠지?', value: 'p' }
+      { text: 'Honestly I cannot see a lot of roadblocks or issues, it should be a clear path to selling my product and making MONEY!', value: 'j' },
+      { text: 'Bring on the challenges! Mistakes are just opportunities to learn and get better within your business', value: 'p' }
     ]
   },
   {
     number: '05',
-    question: '첫 입사 날!<br>나의 첫 인사말은?',
+    question: 'If you had 26 hours in a day, while everyone else still had 24, how would you spend the extra 2 hours?',
     choices: [
-      { text: '안녕하세요ㅎㅎ 잘 부탁드려요~<br>(부끄러워 >///<)', value: '' },
-      { text: '안녕하세요! 저는 자취하고 있고요,<br>MBTI는 ~~ (빨리 친해져야지 >0<) ', value: '' }
+      { text: "Relaxin and chillaxin and unwinding like it's nobody's business", value: '' },
+      { text: 'More time to work on and better myself? Bring it on! ', value: '' }
     ]
   },
   {
     number: '06',
-    question: '날씨가 좋아~<br>유난히 일하기 싫은 날엔?',
+    question: 'What does the ideal workday look like for you?',
     choices: [
-      { text: '날 좋은데 일이라니.<br>그런데 어쩌겠어? 일해야지!', value: '' },
-      { text: '이럴 때 일할 순 없어!!<br>반차 써버릴 거야!', value: '' }
+      { text: 'I feel satisfied throwing myself into work 100%!', value: '' },
+      { text: 'I spend the day surfing the Web, chatting with coworkers and discussing ideas.', value: '' }
     ]
   },
   {
     number: '07',
-    question: '상사한테 한 소리 들었다.<br>나는?',
+    question: 'Do you feel more happiness when helping others or accomplishing your own goals? ',
     choices: [
-      { text: '더 열심히 해서 잘할 수 있다는<br>사람이란 걸 보여주자!', value: '' },
-      { text: '내가 재능이 없나?<br>퇴사각..', value: '' }
+      { text: 'My goals and interests come first!', value: '' },
+      { text: 'I feel happy watching others accomplish their goals and interests.', value: '' }
     ]
   },
   {
     number: '08',
-    question: '일을 하는 데 너무 어려워ㅠㅠ<br>어떻게 해결하지?',
+    question: 'Do you like organizing schedules or going with the flow?',
     choices: [
-      { text: '시간을 더 끌 순 없지.<br>팀장님한테 당장 물어봐야지!', value: '' },
-      { text: '어떡하지.. 야근하더라도<br>방법을 더 찾아봐야겠다. ', value: '' }
+      { text: 'I want every task to be clearly defined and lined up within my schedule book.', value: '' },
+      { text: 'I can take the day as it comes, no matter what comes up! ', value: '' }
     ]
   },
   {
     number: '09',
-    question: '좋은 곳에서 이직 제안이 왔다!<br>나의 선택은?',
+    question: 'Your initial plan falls through, what do you do?',
     choices: [
-      { text: '인생은 도전이야!<br>좋은 기회라고 생각하고 시작해볼래!', value: '' },
-      { text: '지금 회사도 좋은데..<br>위험 부담을 안고 이직하는 게 맞을까?', value: '' }
+      { text: 'Not to worry, I already have a backup to my backup!', value: '' },
+      { text: "Don't stay calm, panic and AHHHHHHH", value: '' }
     ]
   },
   {
     number: '10',
-    question: '회사에서 진급 대상이 되었다.<br>나의 마음은?',
+    question: "I can't wait to get up for work in the morning, it's something I LOVE!"',
     choices: [
-      { text: '드디어 회사가 날 알아주는구나!<br>더 열심히 해야지!', value: '' },
-      { text: '진급하면 월급도 오르겠지?<br>월급 오르면 ○○ 사야지~ (상상의 나래~)', value: '' }
+      { text: 'Yes!', value: '' },
+      { text: 'No...', value: '' }
     ]
   },
 ]
